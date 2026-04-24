71bb545b213b45779741654cca5a1626d28c54b6
edmondkoeii <ekoei@ucsd.edu>
2026-04-23 21:22:53 -0700

    Update histogram.py

--- hw3/histogram.py
+++ hw3/histogram.py
@@ -5,6 +5,20 @@ def histogram(points, bins):
     avoid looping through all bins for each point.
 
     """
-    # TODO: Implement the histogram function
+    n = len(points)
+    densities = []
     
-    return None
+    point_idx = 0  # single pointer, never resets between bins
+    
+    for (a, b) in bins:
+        count = 0
+        width = b - a
+        
+        # count points in [a, b) by advancing the pointer
+        while point_idx < n and points[point_idx] < b:
+            count += 1
+            point_idx += 1
+        
+        densities.append(count / (n * width))
+    
+    return densities
